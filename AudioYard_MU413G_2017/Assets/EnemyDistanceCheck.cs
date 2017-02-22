534bd78dd05cee2b162b09ce49bf61f639575069
SimonPalmblad <simon.palmblad@live.se>
2017-02-22 18:49:43 +0100

    Implemented footsteps, Distance check (not working)

---
+++ AudioYard_MU413G_2017/Assets/EnemyDistanceCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDistanceCheck : MonoBehaviour {
+    //public GameObject Player;
+    Vector3 MyVector;
+    public GameObject player;
+    Transform playerPos;
+    float parameter;
+
+    // Use this for initialization
+	void Start ()
+
+    {
+        playerPos = player.GetComponent<Transform>();
+        Debug.Log("Found rtansdform!!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float dist = Vector3.Distance(playerPos.position.normalized, transform.position.normalized);
+        Debug.Log("You are:" + dist + "from enemy.");
+    
+    }
+
+}
